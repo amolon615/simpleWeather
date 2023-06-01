a3bb360cddd3bfbced25617db387a9af366d0127
amolon615 <114776102+amolon615@users.noreply.github.com>
2023-06-01 15:28:45 +0200

    Initial Commit

---
+++ simpleWeather/simpleWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleWeatherApp.swift
+//  simpleWeather
+//
+//  Created by Artem on 01/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct simpleWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
